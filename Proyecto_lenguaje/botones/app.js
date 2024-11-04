9346b79ed45579a60dfcd7337665afc8c7eef147
sergiioponce0 <sergioponcecastro1@gmail.com>
2024-11-04 13:29:05 +0100

    tengo que hacer los botones para las imagenes

--- Proyecto_lenguaje/botones/app.js
+++ Proyecto_lenguaje/botones/app.js
@@ -1,16 +1,12 @@
-let openBtn = document.querySelector('#open');
-let closeBtn = document.querySelector('#close');
-let modal = document.querySelector('.modal');
-let overlay = document.querySelector('.overlay');
-
-openBtn.addEventListener("click", () => {
-    modal.style.display = "block";
-    overlay.style.display = "block"; // Cambiado a block
-    overlay.style.opacity = "1"; // Cambiado a 1 para mayor claridad
-});
-
-closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-    overlay.style.display = "none"; // Cambiado a none
-    overlay.style.opacity = "0"; // Opcional, ya que se oculta
-});
+document.getElementById('dot1').addEventListener('click', function() {
+    document.getElementById('carImage').src = 'coche1.jpg';
+  });
+  
+  document.getElementById('dot2').addEventListener('click', function() {
+    document.getElementById('carImage').src = 'coche2.jpg';
+  });
+  
+  document.getElementById('dot3').addEventListener('click', function() {
+    document.getElementById('carImage').src = 'coche3.jpg';
+  });
+  
