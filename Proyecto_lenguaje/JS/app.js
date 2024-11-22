041dd74de0275553535c318fdec0589f3c4d2670
sergiioponce0 <sergioponcecastro1@gmail.com>
2024-11-22 19:20:38 +0100

    fin

--- Proyecto_lenguaje/JS/app.js
+++
@@ -1,7 +0,0 @@
-
-  const carImage = document.getElementById('car-image');
-  const changeImageBtn = document.getElementById('change-image-btn');
-
-  changeImageBtn.addEventListener('click', () => {
-    carImage.url = './Proyecto_lenguaje/fotos/bmw.webp';
-  });
